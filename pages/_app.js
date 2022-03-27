50b0b4cc09b803d91872888ecd4cbff0c97a6440
Arafatmollik1 <arafathossain923@gmail.com>
2022-03-28 01:37:35 +0300

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,27 @@
 import "../styles/main.scss";
-
+import Script from "next/script"
 import React from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return(
+    <>
+    <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-2WZ4Q3ST0Q`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2WZ4Q3ST0Q');
+                `}
+      </Script> 
+  
+  <Component {...pageProps} />;
+  </>
+  );
 }
 
 export default MyApp;
